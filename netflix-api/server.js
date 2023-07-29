7e1d064a24b4867a4583d1c6d3a419cc0741f6d7
Shreyasya Tripathi <113131669+shreyasya07@users.noreply.github.com>
2023-07-29 17:53:37 +0530

    Update server.js

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -5,7 +5,12 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+));
 app.use(express.json());
 
 mongoose
